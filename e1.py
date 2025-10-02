7d6a4cea2f95b636304fc5b42a5d7d12301d48bc
Pyetronunes <pyetronunes793@gmail.com>
2025-10-02 14:42:24 -0300

    Lista1 Python-SALA/e1.py

---
+++ e1.py
@@ -0,0 +1,8 @@
+tempo=float(input('Digite quanto tempo você levou na viagem: '))
+velocidade=float(input('Digite qual foi a velocidade média: '))
+distancia= velocidade*tempo
+litros_usados= distancia/12
+print('Velocidade média: ', velocidade)
+print('Tempo gasto: ', tempo)
+print('Distancia percorrida: ', distancia)
+print('Quantidade de litros usados: ', litros_usados)
